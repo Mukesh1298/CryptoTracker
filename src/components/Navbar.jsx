dedb7ee6a6f8cc3c98ab26b9c1bcd2ad11ce7071
Mukesh 1298 <mukeshsinghrana39@gmail.com>
2026-04-24 23:30:34 +0530

    again fix favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar(){
         <>
 <div className="bg-[#1e3eca] w-full flex items-center justify-between mb-6 mr-0 text-white">
         <div className="flex items-center ">
-        <img src="../public/favicon.webp" alt="logo" className="h-16 w-16 rounded-full mr-6" />
+        <img src="/favicon.webp" alt="logo" className="h-16 w-16 rounded-full mr-6" />
         <h1 className="text-xl font-bold">CryptoTrack</h1>
         </div>
   
